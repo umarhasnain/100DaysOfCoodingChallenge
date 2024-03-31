90183fb9db60b7e625f783e02e7ac258a8f63a45
Umar Hasnain <103918550+umarhasnain@users.noreply.github.com>
2024-04-01 01:20:00 +0500

    Add files via upload

---
+++ Day15/app.js
@@ -0,0 +1,21 @@
+let user = " Hello, Umar Hasnain";
+function userName(a) {
+  alert(a);
+}
+userName(user);
+
+var temp = + prompt("Enter a Temperature")
+
+ switch(dayOfWk) {
+     case "temp > 40" :
+     alert("It is to Hot Outside")
+     break;
+     case "temp > 30" :
+        alert("TOday's Weather is Cool")
+     break;
+     case "temp > 20" :
+        alert("OMG!  Today Weather is cool")
+     break;
+     default :
+     alert("Check Again!");
+     }
