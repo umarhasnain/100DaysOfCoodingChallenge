7cae9904cc3cad3fa1fef4e0230c6efb6211c2c1
Umar Hasnain <103918550+umarhasnain@users.noreply.github.com>
2024-04-01 23:50:40 +0500

    Add files via upload

---
+++ Day16/app.js
@@ -0,0 +1,18 @@
+
+let student ={
+    name:"Umar",
+    rollNo:"1234",
+    learning:"JavaScript",
+}
+
+document.write(student.name)
+document.write(student.rollNo)
+document.write(student.learning)
+
+ var plan1 = {
+     name: "Basic",
+     price: 3.99,
+     space: 100,
+     transfer: 1000,
+     pages: 10
+     };
